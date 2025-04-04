020ac4c23e91c17f42836c7eebad3beb764899bc
ankit <sahuankit.murhu@gmail.com>
2025-04-05 01:03:52 +0530

    add: Mapping urls and retriving

--- store/store_service.go
+++ store/store_service.go
@@ -21,7 +21,7 @@ var (
 const cacheDuration = 6 * time.Hour
 
 // Initializing the store service and return a store pointer
-func initaliseStore() *StorageService {
+func InitaliseStore() *StorageService {
 	redisClient := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6359",
 		Password: "",
@@ -36,4 +36,19 @@ func initaliseStore() *StorageService {
 	fmt.Printf("\nRedis Started successfully: pong message = {%s}", pong)
 	storeService.redisClient = redisClient
 	return storeService
+}
+
+func SaveUrlMapping(shortUrl string, longUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, longUrl, cacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, longUrl))
+	}
+}
+
+func RetriveInitialUrl(shortUrl string) string {
+	res, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed retrieving key url | Error: %v - shortUrl: %s\n", err, shortUrl))
+	}
+	return res
 }
