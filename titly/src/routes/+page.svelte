97925b4a7041ea14852401bec22fa7358fdbcd61
ankit <sahuankit.murhu@gmail.com>
2025-04-05 11:08:56 +0530

    add: Font and Background added

--- titly/src/routes/+page.svelte
+++ titly/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="absolute top-0 -z-10 h-full w-full bg-white">
+	<div
+		class="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"
+	></div>
+</div>
+<section class="h-screen w-full flex flex-col items-center justify-center text-zinc-900">
+	<h1 class="text-4xl font-extrabold">Welcome to <span class="text-yellow-500">Titly</span></h1>
+	<p class="text-lg mt-4">Blazing fast URL Shortner</p>
+</section>
