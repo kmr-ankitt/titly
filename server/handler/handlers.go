1cce0b809182d7e7a4b243b7ef193ecd8a941be5
ankit <sahuankit.murhu@gmail.com>
2026-08-24 18:48:43 +0530

    feat: sqlite methods defination

--- server/handler/handlers.go
+++ server/handler/handlers.go
@@ -1,36 +1,37 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/kmr-ankitt/titly/shortner"
+	"github.com/kmr-ankitt/titly/generator"
 	"github.com/kmr-ankitt/titly/store"
 )
 
-type UrlCreationRequest struct {
+type ShortUrlRequest struct {
 	LongUrl string `json:"long_url" binding:"required"`
 }
 
-func CreateShortUrl(ctx * gin.Context){
-	var creationRequest UrlCreationRequest
-	if err := ctx.ShouldBindJSON(&creationRequest); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+func CreateShortUrl(ctx *gin.Context, db *store.StoreService) {
+	var req ShortUrlRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-	
-	shortUrl := shortner.GenerateShortURL(creationRequest.LongUrl)
-	go store.SaveUrlMapping(shortUrl, creationRequest.LongUrl)
-	
-	host := "http://localhost:4000/"
-	ctx.JSON(200, gin.H{
-		"message": "Short URL generated successfully",
-		"short_url": host + shortUrl,
-	})
+
+	longUrl := req.LongUrl
+
+	// check if the long URL already exists in the database
+	if store.ExistsInSqliteStore(db.SqliteClient, longUrl) {
+		shortUrl := store.GetShortUrlFromSqliteStore(db.SqliteClient, longUrl)
+		ctx.JSON(200, gin.H{"short_url": shortUrl})
+		return
+	}
+
+	shortUrl := generator.GenerateShortURL(longUrl)
+	// store.StoreMappingInSqliteStore(db.SqliteClient, longUrl, shortUrl)
+
+	ctx.JSON(200, gin.H{"short_url": shortUrl})
 }
 
-func HandleShortUrlRedirect(ctx * gin.Context){
-	shortUrl := ctx.Param("short-url")
-	longUrl := store.RetriveInitialUrl(shortUrl)
-	ctx.Redirect(302, longUrl)
+func HandleShortUrlRedirect(ctx *gin.Context) {
 }
--- server/store/sqlite.go
+++ server/store/sqlite.go
@@ -15,7 +15,7 @@ func InitaliseSqliteStore() *sql.DB {
 
 	schema := `
 	CREATE TABLE IF NOT EXISTS url_mappings (
-		id INTEGER PRIMARY KEY,
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		long_url TEXT NOT NULL,
 		short_url TEXT NOT NULL UNIQUE,
 		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -30,3 +30,33 @@ func InitaliseSqliteStore() *sql.DB {
 	return db
 }
 
+func ExistsInSqliteStore(db *sql.DB, longUrl string) bool {
+	var exists bool
+	query := "SELECT EXISTS(SELECT 1 FROM url_mappings WHERE long_url = ?);"
+
+	err := db.QueryRow(query, longUrl).Scan(&exists)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return exists
+}
+
+func GetShortUrlFromSqliteStore(db *sql.DB, longUrl string) string {
+	var shortUrl string
+
+	query := "SELECT short_url from url_mappings WHERE long_url = ?;"
+
+	err := db.QueryRow(query, longUrl).Scan(&shortUrl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return shortUrl
+}
+
+func StoreMappingInSqliteStore(db *sql.DB, longUrl string, shortUrl string) {
+
+}
