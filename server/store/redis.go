9ccd6f143c5e91952fddd5723f724f5f590dc8fd
ankit <sahuankit.murhu@gmail.com>
2026-08-26 12:51:59 +0530

    feat: Url caching in redis

--- server/store/redis.go
+++ server/store/redis.go
@@ -32,3 +32,42 @@ func TestRedisConnection(ctx context.Context, rdb *redis.Client) {
 	fmt.Println("foo", val) // >>> foo bar
 
 }
+
+func GetLongUrlFromRedisStore(ctx context.Context, rdb *redis.Client, shortUrl string) (string, error) {
+	longUrl, err := rdb.Get(ctx, shortUrl).Result()
+	if err == redis.Nil {
+		return "", err
+	} else if err != nil {
+		panic(err)
+	} 
+
+	return longUrl, nil
+}
+
+func PutUrlMappingInRedisStore(ctx context.Context, rdb *redis.Client, shortUrl string, longUrl string) error {
+	err := rdb.Set(ctx, shortUrl, longUrl, 604800).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//TODO: Just for testing
+func GetAllMappingsFromRedisStore(ctx context.Context, rdb *redis.Client) (map[string]string, error) {
+	keys, err := rdb.Keys(ctx, "*").Result()
+	if err != nil {
+		return nil, err
+	}
+
+	mappings := make(map[string]string)
+	for _, key := range keys {
+		longUrl, err := rdb.Get(ctx, key).Result()
+		if err != nil {
+			return nil, err
+		}
+		mappings[key] = longUrl
+	}
+
+	return mappings, nil
+}
